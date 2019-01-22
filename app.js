6aa4916a817fe682d68968c2eb9fa914a766f91f
nictengkk <nictengkk@gmail.com>
2019-01-23 03:22:17 +0800

    added form to add new friend, added post instructions, installed bodyParser

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+var friends = ["Vig", "Chester", "FAMF", "Ammar", "Theo"];
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
 app.get("/friends", function(req, res) {
-  var friends = ["Vig", "Chester", "FAMF", "Ammar", "Theo"];
   res.render("friends", { friends: friends });
+});
+
+app.post("/addfriends", function(req, res) {
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
 });
 
 app.listen(5500, function(req, res) {
